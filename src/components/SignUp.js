dc844f9ea78d6ad95186c6fb87efe4c60383e117
rreckonerr <rreckonerr@gmail.com>
2018-08-27 13:05:28 +0700

    add Sign Up input fields

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 import * as routes from '../constants/routes';
 
 const SignUpPage = () =>
-  <div>
-    <h1>SignUp</h1>
-    <SignUpForm />
-  </div>
+    <div>
+        <h1>SignUp</h1>
+        <SignUpForm />
+    </div>
 
 const INITIAL_STATE = {
     username: '',
@@ -15,38 +15,78 @@ const INITIAL_STATE = {
     passwordOne: '',
     passwordTwo: '',
     error: null,
-  };
+};
+
+const byPropKey = (propertyName, value) => () => ({
+    [propertyName]: value,
+});
 
 class SignUpForm extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = { ...INITIAL_STATE };
-  }
+        this.state = { ...INITIAL_STATE };
+    }
 
-  onSubmit = (event) => {
+    onSubmit = (event) => {
 
-  }
+    }
 
-  render() {
-    return (
-      <form onSubmit={this.onSubmit}>
+    render() {
+        const {
+            username,
+            email,
+            passwordOne,
+            passwordTwo,
+            error,
+        } = this.state;
 
-      </form>
-    );
-  }
+        return (
+            <form onSubmit={this.onSubmit}>
+                <input
+                    value={username}
+                    onChange={event => this.setState(byPropKey('username', event.target.value))}
+                    type="text"
+                    placeholder="Full Name"
+                />
+                <input
+                    value={email}
+                    onChange={event => this.setState(byPropKey('email', event.target.value))}
+                    type="text"
+                    placeholder="Email Address"
+                />
+                <input
+                    value={passwordOne}
+                    onChange={event => this.setState(byPropKey('passwordOne', event.target.value))}
+                    type="password"
+                    placeholder="Password"
+                />
+                <input
+                    value={passwordTwo}
+                    onChange={event => this.setState(byPropKey('passwordTwo', event.target.value))}
+                    type="password"
+                    placeholder="Confirm Password"
+                />
+                <button type="submit">
+                    Sign Up
+        </button>
+
+                {error && <p>{error.message}</p>}
+            </form>
+        );
+    }
 }
 
 const SignUpLink = () =>
-  <p>
-    Don't have an account?
+    <p>
+        Don't have an account?
     {' '}
-    <Link to={routes.SIGN_UP}>Sign Up</Link>
-  </p>
+        <Link to={routes.SIGN_UP}>Sign Up</Link>
+    </p>
 
 export default SignUpPage;
 
 export {
-  SignUpForm,
-  SignUpLink,
+    SignUpForm,
+    SignUpLink,
 };
